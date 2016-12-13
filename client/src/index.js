9cdea921dc0ddea5877116f99e380a721f0e1d58
Clément Bouscasse <clement.bouscasse@res-ltd.com>
2016-12-13 17:04:22 +0000

    Allow styles to be overwritten on each deployment

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router/BrowserRouter';
 import App from './App';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+
+// use default bootstrap theme unless a custom one is provided by the user
+if (!process.env.REACT_APP_BOOTSTRAP_CSS_PATH) {
+  require('bootstrap/dist/css/bootstrap.css');
+} else {
+  require(process.env.REACT_APP_BOOTSTRAP_CSS_PATH);
+}
+
+if (!process.env.REACT_APP_APPLICATION_STYLES_PATH) {
+  require('./index.css');
+} else {
+  require(process.env.REACT_APP_APPLICATION_STYLES_PATH);
+}
+
 import 'font-awesome/css/font-awesome.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
