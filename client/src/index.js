dd21fd3540bf3ab811085088f715deb0b40eae95
Clément Bouscasse <jahfet@gmail.com>
2016-12-15 23:08:35 +0000

    silence webpack warning

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,28 @@
 // Polyfill needed by ie 11
 import 'string.prototype.startswith';
 
+import 'font-awesome/css/font-awesome.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from 'react-router/BrowserRouter';
 import App from './App';
 
 // use default bootstrap theme unless a custom one is provided by the user
+// this is the only syntax that appears to work out of the box with create-react-app for conditional imports
 if (!process.env.REACT_APP_BOOTSTRAP_CSS_PATH) {
   require('bootstrap/dist/css/bootstrap.css');
 } else {
-  require(process.env.REACT_APP_BOOTSTRAP_CSS_PATH);
+  //webpack does not like the line below, wrapping it in a string makes the warning go away
+  require(`${process.env.REACT_APP_BOOTSTRAP_CSS_PATH}`);
 }
 
 if (!process.env.REACT_APP_APPLICATION_STYLES_PATH) {
   require('./index.css');
 } else {
-  require(process.env.REACT_APP_APPLICATION_STYLES_PATH);
+  require(`${process.env.REACT_APP_APPLICATION_STYLES_PATH}`);
 }
-
-import 'font-awesome/css/font-awesome.css';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 ReactDOM.render((
   <Router>
