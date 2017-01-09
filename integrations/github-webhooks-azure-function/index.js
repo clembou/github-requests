4e38db356b0d5a349dc6e1d12a6edde3305c5fc4
Polys Georgiou <georgiou.polys@gmail.com>
2017-01-09 14:36:34 +0000

    Add error messages to `context.done()`

--- integrations/github-webhooks-azure-function/index.js
+++ integrations/github-webhooks-azure-function/index.js
@@ -3,28 +3,44 @@ var request = require('request');
 
 module.exports = function (context, data) {
 
-    const REQUESTS_APP_WEBHOOKS_ENDPOINT = process.env['REQUESTS_APP_WEBHOOKS_ENDPOINT'];
-    const AAD_TENANT_GUID = process.env['REACT_APP_TENANT_ID'];
-    const AAD_REQUESTS_APP_CLIENT_ID = process.env['REACT_APP_CLIENT_ID'];
-    const AAD_REQUESTS_APP_CLIENT_SECRET = process.env['REQUESTS_APP_CLIENT_SECRET'];
-
-    // do nothing if required config settings are missing
-    if (!REQUESTS_APP_WEBHOOKS_ENDPOINT || !AAD_TENANT_GUID || !AAD_REQUESTS_APP_CLIENT_ID || !AAD_REQUESTS_APP_CLIENT_SECRET) {
+    // do nothing if the incoming data is empty
+    if (!data) {
         context.done();
+        return;
+    }
+
+    const REQUESTS_APP_WEBHOOKS_ENDPOINT = process.env['REQUESTS_APP_WEBHOOKS_ENDPOINT'];
+    if (!REQUESTS_APP_WEBHOOKS_ENDPOINT || REQUESTS_APP_WEBHOOKS_ENDPOINT.length < 1) {
+        context.done(`'REQUESTS_APP_WEBHOOKS_ENDPOINT' is either missing or empty`);
+        return;
+    }
+
+    const AAD_TENANT_GUID = process.env['REACT_APP_TENANT_ID'];
+    if (!AAD_TENANT_GUID || AAD_TENANT_GUID.length < 1) {
+        context.done(`'AAD_TENANT_GUID' is either missing or empty`);
+        return;
+    }
+
+    const AAD_REQUESTS_APP_CLIENT_ID = process.env['REACT_APP_CLIENT_ID'];
+    if (!AAD_REQUESTS_APP_CLIENT_ID || AAD_REQUESTS_APP_CLIENT_ID.length < 1) {
+        context.done(`'AAD_REQUESTS_APP_CLIENT_ID' is either missing or empty`);
+        return;
+    }
+
+    const AAD_REQUESTS_APP_CLIENT_SECRET = process.env['REQUESTS_APP_CLIENT_SECRET'];
+    if (!AAD_REQUESTS_APP_CLIENT_SECRET || AAD_REQUESTS_APP_CLIENT_SECRET.length < 1) {
+        context.done(`'REQUESTS_APP_CLIENT_SECRET' is either missing or empty`);
         return;
     }
 
     const GITHUB_EVENTS_TO_WATCH = (process.env['GITHUB_EVENTS_TO_WATCH'] || '').split(';');
-    const GITHUB_LOGINS_TO_WATCH = (process.env['REACT_APP_GITHUB_BOT_LOGIN'] || '').split(';');
-
-    // do nothing if there's nothing to watch
-    if (!GITHUB_EVENTS_TO_WATCH || !GITHUB_LOGINS_TO_WATCH || GITHUB_EVENTS_TO_WATCH.length < 1 || GITHUB_LOGINS_TO_WATCH.length < 1) {
+    if (!GITHUB_EVENTS_TO_WATCH || GITHUB_EVENTS_TO_WATCH.length < 1) {
         context.done();
         return;
     }
 
-    // do nothing if the incoming data is empty
-    if (!context.req || !data) {
+    const GITHUB_LOGINS_TO_WATCH = (process.env['REACT_APP_GITHUB_BOT_LOGIN'] || '').split(';');
+    if (!GITHUB_LOGINS_TO_WATCH || GITHUB_LOGINS_TO_WATCH.length < 1) {
         context.done();
         return;
     }
