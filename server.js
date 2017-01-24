0d98c7264a78894fbed253e8711bf0ba8c89ba0c
Polys Georgiou <georgiou.polys@gmail.com>
2017-01-24 16:34:44 +0000

    Remove the `X-Powered-By` header added by Express
    The `X-Powered-By` header provides no real benefit and also leaks
    information about the design of the underlying system that may be
    useful to attackers.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const app = require('./app');
+
+// remove the automatically added `X-Powered-By` header
+app.disable('x-powered-by');
+
 require('./api.js')(app);
 require('./static.js')(app);
 
