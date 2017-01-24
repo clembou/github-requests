6d4490825cea18c1882458be590840c924036c71
Polys Georgiou <georgiou.polys@gmail.com>
2017-01-24 16:45:35 +0000

    Disable caching by default
    Adding the recommended `Cache-Control` header to all responses, as
    well as `Pragma` for legacy HTTP/1.0 clients.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.disable('x-powered-by');
 // this needs to be added first so that headers are added to all subsequent responses
 app.use(function (req, res, next) {
 
+  // disable caching
+  res.header('Cache-Control', 'no-cache, must-revalidate, max-age=0');
+  res.header('Pragma', 'no-cache');
+
   // security headers
   // see https://www.owasp.org/index.php/OWASP_Secure_Headers_Project
   res.header('X-XSS-Protection', '1; mode=block');
