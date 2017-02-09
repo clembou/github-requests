f3aac36c38cc6807b4b907bc02f55b183187c5f3
Clément Bouscasse <clement.bouscasse@res-ltd.com>
2017-02-09 19:03:27 +0000

    read .env file from client folder
    to avoid unnecessary duplication

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config()
+require('dotenv').config({path: './client/.env'})
 const app = require('./app');
 
 // remove the automatically added `X-Powered-By` header
