e2b4b537e6b7b9dc2a70b0a0406eec942ece116c
Cedd Burge <cedd.burge@res-ltd.com>
2018-04-19 14:56:48 +0100

    Make first steps in adding authentication middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,45 +1,45 @@
-// const passport = require('passport');
-// const OIDCBearerStrategy = require('passport-azure-ad').BearerStrategy;
-// const config = require('../config');
+const passport = require('passport');
+const OIDCBearerStrategy = require('passport-azure-ad').BearerStrategy;
+const config = require('../config');
 
-// //const authConfig = require('../config').auth;
+//const authConfig = require('../config').auth;
 
-// module.exports = (app) => {
-//     // OpenID Connect Bearer Token
+module.exports = (appOrRouter) => {
+    // OpenID Connect Bearer Token
 
-//     app.use(passport.initialize());
+    appOrRouter.use(passport.initialize());
 
-//     passport.use(new OIDCBearerStrategy(config.azure, function(token, done) {
-//         // console.log(token)
-//         return done(null, token, null);
-//       }));
+    passport.use(new OIDCBearerStrategy(config.azure, function(token, done) {
+        // console.log(token)
+        return done(null, token, null);
+      }));
       
-//     //   app.use(passport.session());
+    //   app.use(passport.session());
 
-//     // const bearerStrategyOptions = {
-//     //     identityMetadata: authConfig.identityMetadata,
-//     //     clientID: authConfig.clientId,
-//     //     validateIssuer: authConfig.validateIssuer,
-//     //     issuer: authConfig.issuer,
-//     //     passReqToCallback: authConfig.passReqToCallback,
-//     //     isB2C: authConfig.isB2C,
-//     //     policyName: authConfig.policyName,
-//     //     allowMultiAudiencesInToken: authConfig.allowMultiAudiencesInToken,
-//     //     audience: authConfig.audience,
-//     //     loggingLevel: authConfig.loggingLevel,
-//     // };
+    // const bearerStrategyOptions = {
+    //     identityMetadata: authConfig.identityMetadata,
+    //     clientID: authConfig.clientId,
+    //     validateIssuer: authConfig.validateIssuer,
+    //     issuer: authConfig.issuer,
+    //     passReqToCallback: authConfig.passReqToCallback,
+    //     isB2C: authConfig.isB2C,
+    //     policyName: authConfig.policyName,
+    //     allowMultiAudiencesInToken: authConfig.allowMultiAudiencesInToken,
+    //     audience: authConfig.audience,
+    //     loggingLevel: authConfig.loggingLevel,
+    // };
 
-//     // const bearerStrategy = new OIDCBearerStrategy(bearerStrategyOptions, (token, done) => {
-//     //     // `token` is actually the claims dictionary (payload of the JWT token)
-//     //     // console.log('Token: ', token);
-//     //     if (!token.oid) {
-//     //         done(new Error('oid missing from token'));
-//     //     } else {
-//     //         done(null, token);
-//     //     }
-//     // });
+    // const bearerStrategy = new OIDCBearerStrategy(bearerStrategyOptions, (token, done) => {
+    //     // `token` is actually the claims dictionary (payload of the JWT token)
+    //     // console.log('Token: ', token);
+    //     if (!token.oid) {
+    //         done(new Error('oid missing from token'));
+    //     } else {
+    //         done(null, token);
+    //     }
+    // });
 
-//     // passport.use(bearerStrategy);
+    // passport.use(bearerStrategy);
 
-//     return passport.authenticate('oauth-bearer', { session: false });
-// };
+    return passport.authenticate('oauth-bearer', { session: false });
+};
