889995675b41b17a02e489b895b8996e205e0278
Cedd Burge <cedd.burge@res-ltd.com>
2018-04-23 11:54:38 +0100

    Remove github webhook endpoint from api.js now that it is in its own file
    and remove some dead wood and rename variable to indicate that an App or a Router can be passed in

--- api.js
+++ api.js
@@ -1,18 +1,10 @@
 const fs = require('fs');
-const qs = require('qs');
 const https = require('https');
 const url = require('url');
 const request = require('request');
-//const passport = require('passport');
 const config = require('./config');
-const bodyParser = require('body-parser');
-const sgMail = require('@sendgrid/mail');
-const requestUtils = require('./client/src/shared/requestUtils');
-const notifications = require('./notifications');
-const crypto = require('crypto');
-const compare = require('secure-compare');
 
-module.exports = function(app) {
+module.exports = function(appOrRouter) {
   const GITHUB_API_ROOT = 'https://api.github.com';
 
   const getlocalAppRootUrl = request => {
@@ -31,18 +23,6 @@ module.exports = function(app) {
   }
 
   const appData = loadAppData();
-
-  // Utility functions
-  const sendMail = (to, title, body) => {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    const msg = {
-      to: to,
-      from: config.app.emailSender,
-      subject: title,
-      html: body,
-    };
-    return sgMail.send(msg);
-  };
 
   const getProxyRequestOptions = url => ({
     url: GITHUB_API_ROOT + url.replace('/api', ''),
@@ -85,11 +65,11 @@ module.exports = function(app) {
   };
 
   // App end points
-  app.get('/projects', /*passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.get('/projects', function(req, res) {
     res.json(appData);
   });
 
-  app.get('/authenticate/github/:code', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.get('/authenticate/github/:code', function(req, res) {
     const r = request.post(
       {
         url: 'https://github.com/login/oauth/access_token',
@@ -105,7 +85,7 @@ module.exports = function(app) {
   });
 
   // proxy to github api end points
-  app.get('/repos/:organisation/:repo/issues', /*passport.authenticate('oauth-bearer', { session: false }), */function(req, res) {
+  appOrRouter.get('/repos/:organisation/:repo/issues', function(req, res) {
     console.log('Request App received request to: ', req.url);
 
     if (!validateRepository(req.params.organisation, req.params.repo, appData.projects))
@@ -123,14 +103,14 @@ module.exports = function(app) {
     .pipe(res); // pipe response from github back to response from this route / function
   });
 
-  app.get('/repositories/:repoId/issues', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.get('/repositories/:repoId/issues', function(req, res) {
     console.log(`going through pages on repository ${req.params.repoId}`);
     const r = request(getProxyRequestOptions(req.url));
     console.log('Proxied request to: ', getProxyRequestOptions(req.url).url);
     req.pipe(r, genericErrorHandler).on('response', response => rewriteResponseHeaders(req, response)).pipe(res);
   });
 
-  app.get('/repos/:organisation/:repo/issues/:issueId', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.get('/repos/:organisation/:repo/issues/:issueId', function(req, res) {
     if (!validateRepository(req.params.organisation, req.params.repo, appData.projects))
       res.status(403).send({ error: 'Invalid repository name' });
 
@@ -139,7 +119,7 @@ module.exports = function(app) {
     req.pipe(r, genericErrorHandler).pipe(res);
   });
 
-  app.post('/repos/:organisation/:repo/issues', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.post('/repos/:organisation/:repo/issues', function(req, res) {
     if (!validateRepository(req.params.organisation, req.params.repo, appData.projects))
       res.status(403).send({ error: 'Invalid repository name' });
 
@@ -148,7 +128,7 @@ module.exports = function(app) {
     req.pipe(r, genericErrorHandler).pipe(res);
   });
 
-  app.get('/repos/:organisation/:repo/issues/:issueId/comments', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.get('/repos/:organisation/:repo/issues/:issueId/comments', function(req, res) {
     if (!validateRepository(req.params.organisation, req.params.repo, appData.projects))
       res.status(403).send({ error: 'Invalid repository name' });
 
@@ -157,7 +137,7 @@ module.exports = function(app) {
     req.pipe(r, genericErrorHandler).pipe(res);
   });
 
-  app.post('/repos/:organisation/:repo/issues/:issueId/comments', /* passport.authenticate('oauth-bearer', { session: false }), */ function(req, res) {
+  appOrRouter.post('/repos/:organisation/:repo/issues/:issueId/comments', function(req, res) {
     if (!validateRepository(req.params.organisation, req.params.repo, appData.projects))
       res.status(403).send({ error: 'Invalid repository name' });
 
@@ -166,108 +146,4 @@ module.exports = function(app) {
     req.pipe(r, genericErrorHandler).pipe(res);
   });
 
-  // Calculate the X-Hub-Signature header value.
-  function getSignature(buf) {
-    var hmac = crypto.createHmac("sha1", config.github.webHookSecret);
-    hmac.update(buf, "utf-8");
-    return "sha1=" + hmac.digest("hex");
-  }
-
-  // Verify function compatible with body-parser to retrieve the request payload.
-  // Read more: https://github.com/expressjs/body-parser#verify
-  function verifyRequest(req, res, buf, encoding) {
-    var expected = req.headers['x-hub-signature'];
-    var calculated = getSignature(buf);
-    console.log("X-Hub-Signature:", expected, "Content:", "-" + buf.toString('utf8') + "-");
-    if (!compare(expected,calculated)) {
-      throw new Error("Invalid signature.");
-    } else {
-      console.log("Valid signature!");
-    }
-  }
-
-  // We only send emails for issues that are created from the app. 
-  // People that have created issues directly on GitHub will probably be programmers, and will definitely receive a notification directly from GitHub
-  // Issues that are created through this app use the associated bot account, and hence the original creator needs to get an email.
-  function isGitHubWebHookRequestValid(req)  {
-    return ['issues', 'issue_comment'].includes(req.headers['x-github-event']) && userLoginFromGitHubRequestBody(req.body) === config.github.botLogin;
-  }
-
-  function userLoginFromGitHubRequestBody(body){
-    return (body && body.issue.user) ? body.issue.user.login : "";
-  }
-
-  function handleInvalidGitHubWebHookRequest(req, res)  {
-    console.log('RequestApp: The following github webhook was not valid:');
-    console.log(req.headers['x-github-event']);
-    console.log(config.github.botLogin);
-    console.log(userLoginFromGitHubRequestBody(req.body));
-    
-    res.status(422).send({
-      message: 'The github webhook request is not valid'
-    });
-  }
-
-
-  function sendEmailsForGitHubWebHook(req, res)  {
-    try {
-      trySendEmailsForGitHubWebHook(req, res);
-    } catch (error) {
-      handleErrorForEmailsForGitHubWebHook(req, res, error);
-    }
-  }
-    
-  function trySendEmailsForGitHubWebHook(req, res)  {
-    const { email } = requestUtils.getCreator(req.body.issue);
-    const project = notifications.findProject(req.body, appData.projects);
-    const requestUrl = notifications.getRequestUrl(req.body.issue.number, project, getlocalAppRootUrl(req));
-    const eventType = req.headers['x-github-event'];
-    const { subject, content } = notifications.getNotificationText(eventType, req.body, project.name, requestUrl);
-
-    console.log(`Request app: Email notification starting ${email}, ${subject}, ${content}`);
-
-    if (email && subject && content) {
-      sendMail(email, subject, content)
-        .then(response => {
-          console.log(`Request app: Email notification sent successfully to ${email}`);
-        })
-        .catch(error => {
-          //error is an instance of SendGridError
-          //The full response is attached to error.response
-          console.log(error.response);
-        });
-    }
-
-    console.log(`Request app: Email notification returning response`);
-    res.json({ message: 'web hook processed' });
-  }
-    
-  function handleErrorForEmailsForGitHubWebHook(req, res, error)  {
-    console.log('RequestApp: An error occcured while attempting to process the following github webhook:');
-    console.log(error);
-
-    res.status(500).json({
-      message: 'An error occcured while attempting to process a github webhook',
-    });
-  }
-
-  // handle github web hooks
-  app.post('/github-webhooks', [bodyParser.json({verify:verifyRequest})], function(req, res) {
-
-    // this message is unsecured because github doesn't have a token to call us
-    // instead we check that this message was generated by our github instance
-    // by checking that the signature of the body was created with the correct secret
-    console.log(`RequestApp: received web hook action: ${req.headers['x-github-event']}`);
-    //console.log(JSON.stringify(req.headers));
-    //console.log(JSON.stringify(req.body));
-
-    //const valid = isGitHubWebHookRequestValid(req);
-    //console.log(isGitHubWebHookRequestValid(req));
-
-    if (isGitHubWebHookRequestValid(req)) {
-      sendEmailsForGitHubWebHook(req, res)
-    } else {
-      handleInvalidGitHubWebHookRequest(req, res);
-    }
-  });
 };
