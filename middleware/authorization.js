e276fa1558a9bb4bb5177d13cd4828d7d2782710
Cedd Burge <cedd.burge@res-ltd.com>
2018-04-23 11:47:23 +0100

    Remove unused authorization file

--- middleware/authorization.js
+++
@@ -1,8 +0,0 @@
-// module.exports = (router) => {
-//     router.use((req, res, next) => {
-//         if (req.isAuthenticated()) {
-//             return next();
-//         }
-//         res.status(403).end(); // Forbidden
-//     });
-// };
