706ffa3906b6cd8cdbb541fc7a2cb009b0cfa59f
Cedd Burge <cedd.burge@res-ltd.com>
2018-04-23 16:05:31 +0100

    Remove dead wood

--- backend/authenticatedroutes/issues.js
+++ backend/authenticatedroutes/issues.js
@@ -1,10 +1,7 @@
 const config = require('../../config');
-// const https = require('https');
-// const url = require('url');
 const request = require('request');
 const { getProxyRequestOptions, genericErrorHandler } = require( './gitHubProxyHelpers');
 const validateRepository = require( './configHelpers');
-
 
 const authenticatedRouter = require('./authenticatedRouter');
 
@@ -22,17 +19,8 @@ authenticatedRouter.get('/repos/:organisation/:repo/issues', function(req, res) 
   
   req
   .pipe(gitHubRequest, genericErrorHandler) // send to git hub
-  //.on('response', response => rewriteResponseHeaders(req, response)) // response from github, monkey with the headers for some reason. Looking at the logs it doesn't seem to do anything
   .pipe(res); // pipe response from github back to response from this route / function
 });
-
-// I don't think this is required.
-// appOrRouter.get('/repositories/:repoId/issues', function(req, res) {
-//   console.log(`going through pages on repository ${req.params.repoId}`);
-//   const r = request(getProxyRequestOptions(req.url));
-//   console.log('Proxied request to: ', getProxyRequestOptions(req.url).url);
-//   req.pipe(r, genericErrorHandler).on('response', response => rewriteResponseHeaders(req, response)).pipe(res);
-// });
 
 authenticatedRouter.post('/repos/:organisation/:repo/issues', function(req, res) {
   if (!validateRepository(req.params.organisation, req.params.repo, config.appData.projects))
