590881b3cb5bd6dc0ffc044c5a1b400a8972db94
Cedd Burge <cedd.burge@res-ltd.com>
2018-04-24 12:00:40 +0100

    Alter paths of static routes after file move

--- backend/dangerousOpenRoutes/static.js
+++ backend/dangerousOpenRoutes/static.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 const { dangerousOpenRouter } = require('./dangerousOpenRouter');
 
-dangerousOpenRouter.use(express.static(path.resolve(__dirname, 'client', 'build')));
+dangerousOpenRouter.use(express.static(path.resolve(__dirname, '../../client', 'build')));
 
 // This route matches everything, so must be added last
 // Always return the main index.html, so react-router render the route in the client
 dangerousOpenRouter.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
 });
